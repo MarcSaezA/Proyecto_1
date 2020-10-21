bea86bf6f26ddf75a6ff368fb4e2782d5c524083
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2020-10-21 17:16:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,68 +1,106 @@
-const express = require("express");
-const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-let gamer = {
-    name: '',
-    surname: '',
-    score: ''
-};
-let respuesta = {
-    error: false,
-    codigo: 200,
-    mensaje: ''
-};
-app.get('/', function (req, res) {
-    respuesta = {
-        error: true,
-        codigo: 200,
-        mensaje: 'Punto de inicio'
-    };
-    res.send(respuesta);
-});
-app.route('/gamer')
-    .get(function (req, res) {
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: ''
-        };
+ const express = require("express");
+ const bodyParser = require('body-parser');
+ const app = express();
+ app.use(bodyParser.urlencoded({ extended: false }));
+ app.use(bodyParser.json());
+// let gamer = {
+//     name: '',
+//     surname: '',
+//     score: ''
+// };
+// let respuesta = {
+//     error: false,
+//     codigo: 200,
+//     mensaje: ''
+// };
+// app.get('/', function (req, res) {
+//     respuesta = {
+//         error: true,
+//         codigo: 200,
+//         mensaje: 'Punto de inicio'
+//     };
+//     res.send(respuesta);
+// });
+// app.route('/gamer')
+//     .get(function (req, res) {
+//         respuesta = {
+//             error: false,
+//             codigo: 200,
+//             mensaje: ''
+//         };
 
-        res.send(respuesta);
-    })
-    .post(function (req, res) {
-        if (!req.body.name || !req.body.surname || !req.body.score) {
-            respuesta = {
-                error: true,
-                codigo: 502,
-                mensaje: 'El campo nombre, apellido y score necesarios'
-            };
-        } else {
-            if (gamer.name == req.body.name && gamer.surname == req.body.surname) {
-                respuesta = {
-                    error: true,
-                    codigo: 503,
-                    mensaje: 'El jugador ya fue creado previamente'
-                };
-            } else {
-                gamer = {
-                    surname: req.body.surname,
-                    score: req.body.score,
-                    name: req.body.name
+//         res.send(respuesta);
+//     })
+//     .post(function (req, res) {
+//         if (!req.body.name || !req.body.surname || !req.body.score) {
+//             respuesta = {
+//                 error: true,
+//                 codigo: 502,
+//                 mensaje: 'El campo nombre, apellido y score necesarios'
+//             };
+//         } else {
+//             if (gamer.name == req.body.name && gamer.surname == req.body.surname) {
+//                 respuesta = {
+//                     error: true,
+//                     codigo: 503,
+//                     mensaje: 'El jugador ya fue creado previamente'
+//                 };
+//             } else {
+//                 gamer = {
+//                     surname: req.body.surname,
+//                     score: req.body.score,
+//                     name: req.body.name
                     
-                };
-                respuesta = {
-                    error: false,
-                    mensaje: 'Jugador creado',
-                    codigo: 200,        
-                    respuesta: gamer
-                };
-            }
-        }
+//                 };
+//                 respuesta = {
+//                     error: false,
+//                     mensaje: 'Jugador creado',
+//                     codigo: 200,        
+//                     respuesta: gamer
+//                 };
+//             }
+//         }
 
-        res.send(respuesta);
-    })
-app.listen(3000, () => {
-    console.log("El servidor está inicializado en el puerto 3000");
+//         res.send(respuesta);
+//     })
+let jugadores=[
+        jugador = {
+            posicion: '1',
+            alias:'jjpez',
+            nombre:'Josephe',
+            apellido:'Pereza',
+            score:'950'
+        }
+        ,
+        jugador = {
+            posicion: '2',
+            alias:'sanzioj',
+            nombre:'Juanjo',
+            apellido:'Santz',
+            score:'850'
+        }
+        ,
+        jugador = {
+            posicion: '3',
+            alias:'uwumaker',
+            nombre:'Martin',
+            apellido:'Martinez',
+            score:'795'
+        }
+    ];   
+let respuesta= {
+    error:'',
+    codigo:'',
+    mensaje:''
+};
+app.get('/ranking',function(req,res)
+{
+    res.send(jugadores);
+});
+
+app.get('/jugador')
+
+
+ app.listen(3000, () => {
+     console.log("El servidor está inicializado en el puerto 3000");
 });
