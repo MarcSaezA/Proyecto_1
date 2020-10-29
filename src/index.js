02c5918fbd8b1750fa236ab84237ba3af3e36e56
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2020-10-29 19:29:16 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,66 +3,6 @@
  const app = express();
  app.use(bodyParser.urlencoded({ extended: false }));
  app.use(bodyParser.json());
-// let gamer = {
-//     name: '',
-//     surname: '',
-//     score: ''
-// };
-// let respuesta = {
-//     error: false,
-//     codigo: 200,
-//     mensaje: ''
-// };
-// app.get('/', function (req, res) {
-//     respuesta = {
-//         error: true,
-//         codigo: 200,
-//         mensaje: 'Punto de inicio'
-//     };
-//     res.send(respuesta);
-// });
-// app.route('/gamer')
-//     .get(function (req, res) {
-//         respuesta = {
-//             error: false,
-//             codigo: 200,
-//             mensaje: ''
-//         };
-
-//         res.send(respuesta);
-//     })
-//     .post(function (req, res) {
-//         if (!req.body.name || !req.body.surname || !req.body.score) {
-//             respuesta = {
-//                 error: true,
-//                 codigo: 502,
-//                 mensaje: 'El campo nombre, apellido y score necesarios'
-//             };
-//         } else {
-//             if (gamer.name == req.body.name && gamer.surname == req.body.surname) {
-//                 respuesta = {
-//                     error: true,
-//                     codigo: 503,
-//                     mensaje: 'El jugador ya fue creado previamente'
-//                 };
-//             } else {
-//                 gamer = {
-//                     surname: req.body.surname,
-//                     score: req.body.score,
-//                     name: req.body.name
-                    
-//                 };
-//                 respuesta = {
-//                     error: false,
-//                     mensaje: 'Jugador creado',
-//                     codigo: 200,        
-//                     respuesta: gamer
-//                 };
-//             }
-//         }
-
-//         res.send(respuesta);
-//     })
 let jugadores=[{
             posicion: '1',
             alias:'jjpez',
@@ -98,10 +38,10 @@ app.get('/ranking',function(req,res)
     res.send(jugadores);
 });
 
-app.get('/jugadores/:alias',function(req,res)
-{
 
-    for(i = 0; i < jugadores.length; i++)
+app.route('/jugadores/:alias')
+    .get(function (req, res) {
+        for(i = 0; i < jugadores.length; i++)
     {
 
         if (jugadores[i].alias==req.params.alias)
@@ -129,39 +69,88 @@ app.get('/jugadores/:alias',function(req,res)
         }
     res.send(respuesta);
     respuesta.error=true;
-});
-app.post(function (req, res) {
-             if (!req.body.nombre || !req.body.apellido || !req.body.score ||!req.body.alias) {
-                respuesta = {
+
+    })
+    .post(function (req, res) {
+        if (!req.body.nombre || !req.body.apellido || !req.body.score) {
+            respuesta = {
+                 error: true,
+                 codigo: 502,
+                 mensaje: 'El campo alias, nombre, apellido y score necesarios'
+             };
+         } else {
+             if (jugadores.nombre == req.body.nombre && jugadores.apellido == req.body.apellido && jugadores.alias==req.params.alias) {
+                 respuesta = {
                      error: true,
-                     codigo: 502,
-                     mensaje: 'El campo alias, nombre, apellido y score necesarios'
+                     codigo: 503,
+                    mensaje: 'El jugador ya fue creado previamente'
                  };
              } else {
-                 if (jugadores.nombre == req.body.nombre && jugadores.apellido == req.body.apellido && jugadores.alias==req.body.alias) {
-                     respuesta = {
-                         error: true,
-                         codigo: 503,
-                        mensaje: 'El jugador ya fue creado previamente'
-                     };
-                 } else {
-                     jugadores = {
-                         apellido: req.body.apellido,
-                         score: req.body.score,
-                         nombre: req.body.nombre
-                        
-                     };
-                     respuesta = {
-                         error: false,
-                         mensaje: 'Jugador creado',
-                         codigo: 200,        
-                         respuesta: jugadores
-                     };
-                 }
+                 jugador = {
+                     apellido: req.body.apellido,
+                     score: req.body.score,
+                     nombre: req.body.nombre,
+                     alias: req.params.alias                    
+                 };
+                 let nuevoJugador = jugadores.push(jugador);
+                 respuesta = {
+                     error: false,
+                     mensaje: 'Jugador creado',
+                     codigo: 200,        
+                     respuesta: jugador
+                 };
              }
+             jugadores.sort((a,b)=>Math.sign(b.score-a.score));
+             jugadores.forEach(function(item,index,array){item.position=index+1;})
+        }
+        res.send(respuesta);
+    })
+    .put(function (req, res) {
+        
+        if (!req.body.nombre || !req.body.apellido || !req.body.score) {
+            respuesta = {
+                 error: true,
+                 codigo: 502,
+                 mensaje: 'El campo alias, nombre, apellido y score necesarios'
+             };
+         } else {
+             var playerexists = false;
+            for(i = 0; i < jugadores.length; i++)
+            {
+                
+                if (jugadores[i].alias==req.params.alias) {
 
+                    jugadores[i] = {
+                        apellido: req.body.apellido,
+                        score: req.body.score,
+                        nombre: req.body.nombre,
+                        alias: req.params.alias 
+                    };
+                    playerexists = true;
+                }  
+            }
+            if (!playerexists){
+                respuesta = {
+                    error: false,
+                    mensaje: 'El jugador no existe',
+                    codigo: 200,        
+                    respuesta: jugadores
+                };
+            }
+            else{
+                respuesta = {
+                    error: false,
+                    mensaje: 'Jugador actualizado',
+                    codigo: 200,        
+                    respuesta: jugadores
+                };
+            }
 
-
+        }
+             jugadores.sort((a,b)=>Math.sign(b.score-a.score));
+             jugadores.forEach(function(item,index,array){item.position=index+1;})
+        res.send(respuesta);
+    })
  app.listen(3000, () => {
      console.log("El servidor está inicializado en el puerto 3000");
 });
