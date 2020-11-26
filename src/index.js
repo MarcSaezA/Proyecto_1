aac57aae703a0f65d05538ae85430aae60cdf368
MarcSaezA <72219572+MarcSaezA@users.noreply.github.com>
2020-11-26 19:06:26 +0100

    Done
    Te falta mirar que es lo que falla aunque en al resto les vaya, miralo en casa

--- src/index.js
+++ src/index.js
@@ -1,68 +1,16 @@
 const express = require("express");
-const bodyParser = require('body-parser');
-const app = express();
+const bodyParser = require("body-parser");
+const app = express()
+const port=process.env.PORT || 3000;
+const api= require("./api.js");
+var swaggerUi= require("swagger-ui-express");
+
+swaggerDoc=require("./swagger.json");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-let gamer = {
-    name: '',
-    surname: '',
-    score: ''
-};
-let respuesta = {
-    error: false,
-    codigo: 200,
-    mensaje: ''
-};
-app.get('/', function (req, res) {
-    respuesta = {
-        error: true,
-        codigo: 200,
-        mensaje: 'Punto de inicio'
-    };
-    res.send(respuesta);
-});
-app.route('/gamer')
-    .get(function (req, res) {
-        respuesta = {
-            error: false,
-            codigo: 200,
-            mensaje: ''
-        };
+api.setup(app);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDoc));
 
-        res.send(respuesta);
-    })
-    .post(function (req, res) {
-        if (!req.body.name || !req.body.surname || !req.body.score) {
-            respuesta = {
-                error: true,
-                codigo: 502,
-                mensaje: 'El campo nombre, apellido y score necesarios'
-            };
-        } else {
-            if (gamer.name == req.body.name && gamer.surname == req.body.surname) {
-                respuesta = {
-                    error: true,
-                    codigo: 503,
-                    mensaje: 'El jugador ya fue creado previamente'
-                };
-            } else {
-                gamer = {
-                    surname: req.body.surname,
-                    score: req.body.score,
-                    name: req.body.name
-                    
-                };
-                respuesta = {
-                    error: false,
-                    mensaje: 'Jugador creado',
-                    codigo: 200,        
-                    respuesta: gamer
-                };
-            }
-        }
-
-        res.send(respuesta);
-    })
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("El servidor está inicializado en el puerto 3000");
 });
